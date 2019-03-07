ca93c819dd8ef016c5846a5c3939e8298e4a1f3e
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-03-07 18:00:58 +0100

    setup earley parser

--- src/parser.jl
+++ src/parser.jl
@@ -18,8 +18,52 @@ export Earley
 struct Earley
 end
 
-function validate()
-    return "Hello World"
+
+# DECLARE ARRAY S;
+s = []
+
+# function INIT(words)
+#     S ← CREATE-ARRAY(LENGTH(words) + 1)
+#     for k ← from 0 to LENGTH(words) do
+#         S[k] ← EMPTY-ORDERED-SET
+function init(tokens)
+    s = [Set() for i in 1:(length(tokens))]
 end
 
+# function EARLEY-PARSE(words, grammar)
+#     INIT(words)
+#     ADD-TO-SET((γ → •S, 0), S[0])
+#     for k ← from 0 to LENGTH(words) do
+#         for each state in S[k] do  // S[k] can expand during this loop
+#             if not FINISHED(state) then
+#                 if NEXT-ELEMENT-OF(state) is a nonterminal then
+#                     PREDICTOR(state, k, grammar)         // non-terminal
+#                 else do
+#                     SCANNER(state, k, words)             // terminal
+#             else do
+#                 COMPLETER(state, k)
+#         end
+#     end
+#     return chart
+function early_parse(tokens,grammar)
+    init(tokens)
+
+end
+
+# procedure PREDICTOR((A → α•Bβ, j), k, grammar)
+#     for each (B → γ) in GRAMMAR-RULES-FOR(B, grammar) do
+#         ADD-TO-SET((B → •γ, k), S[k])
+#     end
+
+# procedure SCANNER((A → α•aβ, j), k, words)
+#     if a ⊂ PARTS-OF-SPEECH(words[k]) then
+#         ADD-TO-SET((A → αa•β, j), S[k+1])
+#     end
+
+# procedure COMPLETER((B → γ•, x), k)
+#     for each (A → α•Bβ, j) in S[x] do
+#         ADD-TO-SET((A → αB•β, j), S[k])
+#     end
+
+# http://matt.might.net/articles/parsing-with-derivatives/
 end
